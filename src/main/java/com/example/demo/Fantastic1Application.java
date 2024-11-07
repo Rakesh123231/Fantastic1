e1044e787b127cc65598eb952ebfb4cafb342895
Rakesh123321 <workwithlove99@gmail.com>
2024-11-07 18:55:34 +0530

    My First Commit

---
+++ src/main/java/com/example/demo/Fantastic1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fantastic1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fantastic1Application.class, args);
+	}
+
+}
