3b9586fc5277f199061a10130b4792c56e787524
Rakesh123321 <workwithlove99@gmail.com>
2024-11-08 01:43:21 +0530

    Added more CRUD operations

--- src/main/java/com/example/demo/services/CitizenService.java
+++ src/main/java/com/example/demo/services/CitizenService.java
@@ -1,9 +1,20 @@
 package com.example.demo.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.example.demo.entity.Citizen;
 
 public interface CitizenService {
 	
 	public Citizen createCitizen (Citizen cz);
+	
+	public List<Citizen> getAllCitizen();
+	
+	public Optional<Citizen> findByCitizenId(int id);
+	
+	public Citizen saveCitizen (Citizen cz);
+	
+	public void deleteCitizen (Citizen cz);
 
 }
